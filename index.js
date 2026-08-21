4a21febf7631f34cde4d7e23de751ef83b13c67b
italianlawrence <314434779+italianlawrence@users.noreply.github.com>
2026-08-21 02:37:40 +0200

    Now the photos are loaded dinamically via Javascript

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+const bodyElement = document.querySelector('body')
+
 const posts = [
     {
         name: "Vincent van Gogh",
@@ -28,3 +31,44 @@ const posts = [
     }
 ]
 
+
+for(let i = 0; i < posts.length; i++) {
+
+    let htmlStructure = `
+
+    <section>
+        <div class = "section-header">
+            <img src = "${posts[i].avatar}">
+
+            <div class = "section-info">
+                <h2 class = "desc-h2"> ${posts[i].name}</h2>
+                <p class = "desc-paragraph"> ${posts[i].location} </p>
+            </div>
+        </div>
+
+        <img class = "small" src = "${posts[i].post}">
+
+        <div class = "logos">
+             <img src = "images/icon-heart.png" alt = 'Leave a like icon'>
+             <img src = "images/icon-comment.png" alt = 'Leave a comment icon'>
+             <img src = "images/icon-dm.png" alt = 'Leave a DM icon'>
+        </div>
+
+        <p class = "likes"> <b> ${posts[i].likes} likes </b> </p>
+
+        <p class = 'img-desc'><b>${posts[i].username}</b> ${posts[i].comment} </p>
+    </section>   
+    
+    `
+
+
+    bodyElement.innerHTML += htmlStructure
+
+
+
+
+
+
+
+
+}
